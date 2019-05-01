840bd1272d17b09acc8c62148313a7f20249fcd1
wakamsha <wakamsha@gmail.com>
2019-05-01 19:45:50 +0900

    :sparkles: Imrove LoginControl

---
+++ packages/getting-started/src/scripts/components/LoginControl.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+
+function UserGreeting(): JSX.Element {
+  return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(): JSX.Element {
+  return <h1>Please sign up.</h1>;
+}
+
+type GreetingProps = {
+  loggedIn: boolean;
+};
+function Greeting(props: GreetingProps): JSX.Element {
+  const { loggedIn } = props;
+  return loggedIn ? <UserGreeting /> : <GuestGreeting />;
+}
+
+function LoginButton(props: { onClick: (e: React.MouseEvent) => void }): JSX.Element {
+  return <button onClick={props.onClick}>Login</button>;
+}
+
+function LogoutButton(props: { onClick: (e: React.MouseEvent) => void }) {
+  return <button onClick={props.onClick}>Logout</button>;
+}
+
+type State = {
+  loggedIn: boolean;
+};
+export class LoginControl extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      loggedIn: false,
+    };
+  }
+
+  private handleLoginClick = () => this.setState({ loggedIn: true });
+  private handleLogoutClick = () => this.setState({ loggedIn: false });
+
+  public render() {
+    const { loggedIn } = this.state;
+
+    return (
+      <>
+        <Greeting loggedIn={loggedIn} />
+        {loggedIn ? <LogoutButton onClick={this.handleLogoutClick} /> : <LoginButton onClick={this.handleLoginClick} />}
+      </>
+    );
+  }
+}
