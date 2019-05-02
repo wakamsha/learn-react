30372e838f5bdd00eb9fb9ddc1b25d8259cb379e
wakamsha <wakamsha@gmail.com>
2019-05-02 20:55:56 +0900

    :sparkles: Improve with useLayoutEffect in Hooks

--- packages/getting-started/src/scripts/components/hooks/HookApp.tsx
+++ packages/getting-started/src/scripts/components/hooks/HookApp.tsx
@@ -5,14 +5,24 @@ type Props = {
 };
 export function HookApp(props: Props) {
   const [count, setCount] = React.useState<number>(props.count);
+  // @MEMO プリミティブ型にしか使えない。
+  // オブジェクト型だと値の変更を検知できないため。その場合ははコールバックref を使うこと
+  const prevCountRef = React.useRef<number>();
+
   const handleClick = () => setCount(count + 2);
+
   React.useEffect(() => {
+    prevCountRef.current = count;
     document.title = `You clicked ${count} times`;
     return () => console.log('再描画');
   });
+
+  React.useLayoutEffect(() => console.log(1111));
+
   return (
     <>
       <p>you clicked {count} times</p>
+      <p>prev count: {prevCountRef.current}</p>
       <button onClick={handleClick}>Click me</button>
     </>
   );
