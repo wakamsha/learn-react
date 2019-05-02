c6128b4beccee62edae685d31998d511785ec0f5
wakamsha <wakamsha@gmail.com>
2019-05-02 16:42:08 +0900

    :sparkles: Improve with Ref

---
+++ packages/getting-started/src/scripts/components/ref/CustomTextInput.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+
+// 原則コールバック ref を使う
+// React.createRef は特に使う必要もなさそう
+export class CustomTextInput extends React.Component {
+  private textInput: HTMLInputElement;
+
+  constructor(props: {}) {
+    super(props);
+  }
+
+  private focusTextInput = () => this.textInput && this.textInput.focus();
+
+  private handleRef = (input: HTMLInputElement) => (this.textInput = input);
+
+  public componentDidMount() {
+    this.focusTextInput();
+  }
+
+  public render() {
+    return (
+      <div>
+        <input type="text" ref={this.handleRef} />
+        <input type="button" value="Focus the text input" onClick={this.focusTextInput} />
+      </div>
+    );
+  }
+}
--- packages/getting-started/src/scripts/index.tsx
+++ packages/getting-started/src/scripts/index.tsx
@@ -6,7 +6,8 @@ import { render } from 'react-dom';
 // import { SignUpDialog } from './components/compositions/SignupDialog';
 // import { FilterableProductTable, PRODUCTS } from './components/thinking/FilterableProductTable';
 // import { ContextApp } from './components/contexts/ContextApp';
-import { CommentList } from './components/hoc/CommentList';
+// import { CommentList } from './components/hoc/CommentList';
+import { CustomTextInput } from './components/ref/CustomTextInput';
 // import { NameForm } from './components/NameForm';
 // import { FlavorForm } from './components/FlavorForm';
 // import { Reservation } from './components/Reservation';
@@ -31,6 +32,7 @@ render(
   // <SignUpDialog />,
   // <FilterableProductTable products={PRODUCTS} />,
   // <ContextApp />,
-  <CommentList />,
+  // <CommentList />,
+  <CustomTextInput />,
   document.getElementById('app'),
 );
