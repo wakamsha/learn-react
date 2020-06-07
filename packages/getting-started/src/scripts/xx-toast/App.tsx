e764d7d683d7e056e9be3bb77bc56278c5ad944b
山田直樹 <wakamsha@gmail.com>
2020-06-07 14:13:33 +0900

    :lipstick: Toast のスタイルを styled-components で実装

--- packages/getting-started/src/scripts/xx-toast/App.tsx
+++ packages/getting-started/src/scripts/xx-toast/App.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export const ToastApp = (): JSX.Element => (
   <ToastProvider>
-    <h1>Toast App</h1>
+    <h1>Snackbar App</h1>
     <AddMessage />
   </ToastProvider>
 );
--- packages/getting-started/src/scripts/xx-toast/components/Toast/Container.tsx
+++ packages/getting-started/src/scripts/xx-toast/components/Toast/Container.tsx
@@ -1,8 +1,9 @@
 import { Item } from './Item';
 import { Toast } from '.';
 import { createPortal } from 'react-dom';
-import { css } from 'emotion';
+// import { css } from 'emotion';
 import React from 'react';
+import styled from 'styled-components';
 
 type Props = {
   toasts: Toast[];
@@ -10,22 +11,34 @@ type Props = {
 
 export const Container = ({ toasts }: Props): JSX.Element =>
   createPortal(
-    <div className={baseStyle}>
+    // <div className={baseStyle}>
+    <StyledBase>
       {toasts.map(({ id, content, theme }) => (
         <Item key={id} id={id} theme={theme}>
           {content}
         </Item>
       ))}
-    </div>,
+    </StyledBase>,
+    // </div>,
     document.body,
   );
 
-const baseStyle = css({
-  position: 'absolute',
-  bottom: 0,
-  left: 0,
-  zIndex: 1,
-  padding: 16,
-  display: 'flex',
-  flexDirection: 'column-reverse',
-});
+const StyledBase = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  z-index: 1;
+  display: flex;
+  flex-direction: column-reverse;
+  padding: 16px;
+`;
+
+// const baseStyle = css({
+//   position: 'absolute',
+//   bottom: 0,
+//   left: 0,
+//   zIndex: 1,
+//   padding: 16,
+//   display: 'flex',
+//   flexDirection: 'column-reverse',
+// });
