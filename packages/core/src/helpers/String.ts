a30d24bc31ea0400eedd20825d1068672ace819d
山田直樹 <wakamsha@gmail.com>
2022-04-09 22:30:08 +0900

    :sparkles: メールアドレス文字列の有効性を検証する util 関数を実装する

--- packages/core/src/helpers/String.ts
+++ packages/core/src/helpers/String.ts
@@ -5,3 +5,13 @@ export const makeId = (() => {
   let id = 0;
   return () => `id-${++id}`;
 })();
+
+/**
+ * 有効なメールアドレスかどうかチェックします。
+ *
+ * @param value 入力文字列。
+ * @returns 有効かどうか。
+ */
+export function isValidEmail(value: string): boolean {
+  return !!value.match(/^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+}
