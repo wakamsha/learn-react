fc7bf9f2857f18a5742039d5bd65859b3b0861e2
YAMADA Naoki <wakamsha@gmail.com>
2022-08-27 19:29:52 +0900

    :sparkles: TextArea コンポーネントを実装する (#606)

--- packages/core/src/components/inputs/TextArea/index.tsx
+++ packages/core/src/components/inputs/TextArea/index.tsx
@@ -1,110 +1,75 @@
 import { css } from '@emotion/css';
-import type { IconName } from '@learn-react/icon';
 import type { ChangeEvent } from 'react';
 import { Duration, FontSize, LineHeight } from '../../../constants/Style';
-import { cssVar, gutter, square } from '../../../helpers/Style';
-import { Icon } from '../../dataDisplay/Icon';
-import { IconButton } from '../IconButton';
+import { cssVar, gutter } from '../../../helpers/Style';
 
 type Props = {
   value: string;
   name: string;
   onChange: (value: string) => void;
   id?: string;
+  /**
+   * @default 1
+   */
+  minRows?: number;
+  /**
+   * @default Infinity
+   */
+  maxRows?: number;
   placeholder?: string;
   disabled?: boolean;
   readonly?: boolean;
-  inputMode?: 'none' | 'text' | 'tel' | 'url' | 'email' | 'numeric' | 'decimal' | 'search';
-  pattern?: string;
   tabIndex?: number;
   autoFocus?: boolean;
-  /** 先頭に表示するアイコン */
-  icon?: IconName;
-  clearable?: boolean;
   /**
    * 入力値が不正かどうか
    *
    * @default false
    */
   invalid?: boolean;
-  onFocus?: (e: ChangeEvent<HTMLInputElement>) => void;
-} & XOR<
-  {
-    type?: 'text' | 'email' | 'password' | 'search' | 'tel' | 'url';
-    maxLength?: number;
-  },
-  {
-    type: 'number';
-    min?: number;
-    max?: number;
-  }
->;
+};
 
 /**
- * テキストフィールドは、ユーザーがテキストを入力・編集する UI です。
+ * TextArea コンポーネントは、複数行のテキスト入力で、ユーザーが大量のテキストを入力する場合に便利です。
  *
- * 一般的には、フォームやダイアログに表示されます。
+ * テキストエリアの高さはコンテンツに応じて自動的に伸縮します。
  *
  * @param props
  */
-export const TextField = ({
+export const TextArea = ({
   value,
   name,
   onChange,
   id,
+  minRows = 1,
+  maxRows = Infinity,
   placeholder,
   disabled,
   readonly,
-  inputMode,
-  pattern,
   tabIndex,
   autoFocus,
-  icon,
-  clearable,
   invalid,
-  onFocus,
-  type,
-  maxLength,
-  min,
-  max,
 }: Props) => {
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    onChange(e.target.value);
-  };
+  const rows = Math.min(Math.max(minRows, value.split(`\n`).length), maxRows);
 
-  const handleClickClear = () => {
-    onChange('');
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    onChange(e.target.value);
   };
 
   return (
     <div className={styleBase} aria-disabled={disabled} aria-invalid={invalid}>
-      {icon ? (
-        <span className={styleIcon} role="presentation">
-          <Icon name={icon} />
-        </span>
-      ) : null}
-      <input
-        autoFocus={autoFocus}
+      <textarea
+        rows={rows}
+        value={value}
+        name={name}
         id={id}
         className={styleInput}
-        name={name}
-        type={type}
-        value={value}
         placeholder={placeholder}
-        disabled={disabled}
         readOnly={readonly}
-        inputMode={inputMode}
-        pattern={pattern}
         tabIndex={tabIndex}
+        autoFocus={autoFocus}
         onChange={handleChange}
-        onFocus={onFocus}
-        maxLength={maxLength}
-        min={min}
-        max={max}
       />
-      {clearable && !disabled && value ? (
-        <IconButton name="times" variant="bare" size="small" onClick={handleClickClear} />
-      ) : null}
     </div>
   );
 };
@@ -126,14 +91,6 @@ const styleBase = css`
     background-color: ${cssVar('ThemePrimaryDark')};
   }
 
-  > * {
-    min-width: 0;
-  }
-
-  > :not(:first-child) {
-    margin-left: ${gutter(2)};
-  }
-
   &[aria-disabled='true'] {
     background-color: ${cssVar('ThemeDisabledLight')};
   }
@@ -153,23 +110,13 @@ const styleBase = css`
   }
 `;
 
-const styleIcon = css`
-  flex: 0 0 auto;
-  ${square(24)}
-
-  > svg {
-    fill: ${cssVar('ThemePrimaryNeutral')};
-  }
-`;
-
 const styleInput = css`
-  display: inline-flex;
-  flex: 1 1 100%;
   width: 100%;
   padding: ${gutter(2)} 0;
   font-size: ${FontSize.Regular};
   line-height: ${LineHeight.Regular};
   color: ${cssVar('TextNeutral')};
+  resize: none;
   background-color: transparent;
   border: none;
   outline: none;
