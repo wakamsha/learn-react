909cb0a2d4eee16dd0993776267c6a151590a30e
wakamsha <wakamsha@gmail.com>
2022-11-08 02:59:51 +0900

    :sparkles: `useTransition` + `Suspense` + `Loadable` のデモを実装する

---
+++ packages/try/src/Transition2/4_WithLoadable.story.tsx
@@ -0,0 +1,91 @@
+import { css } from '@emotion/css';
+import { FontSize } from '@learn-react/core/constants/Style';
+import { cssVar, gutter } from '@learn-react/core/helpers/Style';
+import { Suspense } from 'react';
+import type { IssueType } from './common/Api';
+import { fetchIssues, useData } from './common/Api';
+import { usePageNumberV2 } from './common/usePageNumber';
+
+/**
+ * useSWR の代わりに `useData` + `Loadable` を用いて `3_WithTransition` を再実装した例。
+ */
+export const Story = () => {
+  const { page, isPending, incrementPage, decrementPage } = usePageNumberV2();
+
+  return (
+    <div className={styleBase}>
+      <div>
+        <button disabled={isPending} onClick={decrementPage}>
+          前へ
+        </button>
+        <button disabled={isPending} onClick={incrementPage}>
+          次へ
+        </button>
+        {isPending ? <span className={styleIndicator}>Loading...</span> : null}
+      </div>
+
+      <Suspense fallback={<p>Loading...</p>}>
+        <Presentation page={page} />
+      </Suspense>
+    </div>
+  );
+};
+
+const styleBase = css`
+  > :not(:first-child) {
+    margin-top: ${gutter(4)};
+  }
+`;
+
+const styleIndicator = css`
+  font-weight: bold;
+  color: ${cssVar('ThemeDangerNeutral')};
+`;
+
+const Presentation = ({ page }: { page: number }) => {
+  const issues = useData(`issues:${page}`, fetchIssues, page);
+
+  return (
+    <ul className={styleIssues}>
+      {issues?.map(issue => (
+        <li key={issue.id}>
+          <Issue issue={issue} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const styleIssues = css`
+  > :not(:first-child) {
+    margin-top: ${gutter(2)};
+  }
+`;
+
+type IssueProps = {
+  issue: IssueType;
+};
+
+const Issue = ({ issue }: IssueProps) => (
+  <article className={styleCard}>
+    <h1>
+      <a href={issue.html_url} target="__blank">
+        {issue.title}
+      </a>
+    </h1>
+
+    <p>
+      Opened by{' '}
+      <a href={issue.user.html_url} target="__blank">
+        {issue.user.login}
+      </a>
+    </p>
+  </article>
+);
+
+const styleCard = css`
+  padding: ${gutter(4)};
+  font-size: ${FontSize.Regular};
+  background-color: ${cssVar('TexturePaper')};
+  box-shadow: ${cssVar('ShadowNeutral')};
+`;
---
+++ packages/try/src/Transition2/common/Api.ts
@@ -0,0 +1,82 @@
+import { stringify } from 'qs';
+import { Loadable } from './Loadable';
+
+type Method = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+
+function request<REQ extends {}, RES>({
+  method,
+  path,
+  send,
+  query = {},
+}: {
+  method: Method;
+  path: string;
+  send?: REQ;
+  query?: any;
+}): Promise<RES> {
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  };
+  const queryStrings = Object.keys(query).length ? `?${stringify(query)}` : '';
+  const url = `https://api.github.com${path}${queryStrings}`;
+
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      headers,
+      method,
+      ...(send ? { body: JSON.stringify(send) } : {}),
+    })
+      .then(response => {
+        if (!response.ok) {
+          console.error('エラーレスポンス', response);
+          reject(response);
+        } else {
+          response.json().then((data: RES) => {
+            resolve(data);
+          });
+        }
+      })
+      .catch(error => {
+        console.error(error);
+        reject(error);
+      });
+  });
+}
+
+const dataMap: Map<string, unknown> = new Map();
+
+export function useData<T, P>(cacheKey: string, fetch: (p: P) => Promise<T>, prop: P): T {
+  const cachedData = dataMap.get(cacheKey) as Loadable<any> | undefined;
+
+  if (cachedData === undefined) {
+    const [loadable, promise] = Loadable.newAndGoPromise(fetch(prop));
+    dataMap.set(cacheKey, loadable);
+    throw promise;
+  }
+
+  return cachedData.getOrThrow();
+}
+
+export type IssueType = {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    id: number;
+    login: string;
+    html_url: string;
+  };
+};
+
+export function fetchIssues(page: number) {
+  return request<any, IssueType[]>({
+    method: 'GET',
+    path: '/repos/facebook/react/issues',
+    query: {
+      page,
+      per_page: 10,
+      state: 'all',
+    },
+  });
+}
