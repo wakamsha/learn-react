94347b4ca22e037a9bd0e5c11d05ecb8c4c64522
wakamsha <wakamsha@gmail.com>
2023-01-05 00:27:05 +0900

    :sparkles: scrollbar の表示有無を検証する util 関数を実装する

--- packages/core/src/helpers/Browser.ts
+++ packages/core/src/helpers/Browser.ts
@@ -24,3 +24,13 @@ export function scrollbarSize(): number {
 
   return widthNoScroll - widthWithScroll;
 }
+
+/**
+ * HTML 要素のスクロールバー ( 縦 ) が表示されているかどうかを検証します。
+ *
+ * @param element 検証する HTML 要素。未指定の場合は `document.documentElement` ( `<html />` ) を検証します。
+ * @returns 表示されているかどうか
+ */
+export function isVisibleScrollbarOf(element: HTMLElement = document.documentElement): boolean {
+  return element.scrollHeight > element.clientHeight;
+}
