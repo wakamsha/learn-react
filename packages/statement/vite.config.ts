5337188a461e59e42307020adf84fada2e76a0c3
wakamsha <wakamsha@gmail.com>
2023-03-22 03:34:16 +0900

    :construction: `statement` パッケージのビルドロジックを `builder` パッケージに置き換える

--- packages/statement/vite.config.ts
+++ packages/statement/vite.config.ts
@@ -1,10 +1,15 @@
+import { createUserConfig } from '@learn-react/builder';
+import { resolve } from 'path';
 import { defineConfig, type UserConfig } from 'vite';
-import { createUserConfig } from '../../builder/vite';
 
 export default defineConfig(({ mode }): UserConfig => {
   console.info({ mode });
 
   return createUserConfig({
     basePath: __dirname,
+    alias: {
+      '@learn-react/core': resolve(__dirname, '../core/src'),
+      '@learn-react/icon': resolve(__dirname, '../icon/dist'),
+    },
   });
 });
