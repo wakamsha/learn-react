900b7d3dd030617b0ffab7c7fab790e746d892d7
wakamsha <wakamsha@gmail.com>
2023-03-27 02:34:03 +0900

    :wrench: `icon` パッケージに prettier をインストールする

---
+++ packages/icon/.prettierrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  printWidth: 120, // Extend from default (80) to prevent excessive line breaks.
+  // tabWidth: 2,
+  // useTabs: false,
+  // semi: true,
+  singleQuote: true, // Change from default (false) because it's easier to type and read than double-quote.
+  // quoteProps: 'as-needed',
+  // jsxSingleQuote: false,
+  trailingComma: 'all', // To facilitate reordering object properties.
+  // bracketSpacing: true,
+  // bracketSameLine: false,
+  // arrowParens: 'always',
+  // rangeStart: 0,
+  // rangeEnd: Infinity,
+  // parser: undefined,
+  // filePath: undefined,
+  // requirePragma: false,
+  // insertPragma: false,
+  // proseWrap: 'preserve',
+  // htmlWhitespaceSensitivity: 'css',
+  // vueIndentScriptAndStyle: false,
+  // endOfLine: 'lf',
+  // embeddedLanguageFormatting: 'auto',
+  // singleAttributePerLine: false,
+};
