bf3edfd9099c3de36bc453ff79130c7eb7a524a5
YAMADA Naoki <wakamsha@gmail.com>
2023-06-02 01:04:34 +0900

    useClickOutside を実装する (#1063)
    * :sparkles: `useClickOutside` を実装する
    
    * :cyclone: touch event をサポートする

---
+++ packages/core/src/hooks/useClickOutside/index.ts
@@ -0,0 +1,40 @@
+import { useEffect, useRef, type RefObject } from 'react';
+
+/**
+ * ある要素の領域外をクリックしたイベントを検知します。
+ *
+ * @param callback - 領域外をクリックした時に発火するコールバック関数
+ * @param enabled - 有効化フラグ ( default: true )
+ *
+ * @returns 領域（起点）となる要素。
+ *
+ * @example
+ * ```tsx
+ * const ref = useClickOutside<HTMLDivElement>(() => {
+ *   console.info('clicked outside!');
+ * });
+ *
+ * return <div ref={ref}>Inner area</div>;
+ * ```
+ */
+export function useClickOutside<T extends HTMLElement>(callback: (event: Event) => void, enabled = true): RefObject<T> {
+  const ref = useRef<T>(null);
+
+  useEffect(() => {
+    const listener = (event: Event) => {
+      !ref.current?.contains(event.target as HTMLElement) && callback(event);
+    };
+
+    if (enabled) {
+      document.addEventListener('click', listener);
+      document.addEventListener('touchstart', listener);
+    }
+
+    return () => {
+      document.removeEventListener('click', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [callback, enabled]);
+
+  return ref;
+}
