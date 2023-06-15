3259d7d1ee51d7be867ef4b9f96d2a8d802c7a4e
wakamsha <wakamsha@gmail.com>
2023-06-16 00:52:03 +0900

    :wrench: prettier

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,4 +1,5 @@
 module.exports = {
   './**/*.{js,ts,tsx}': ['cspell', 'prettier -c', 'eslint -c .eslintrc.cjs'],
   './**/*.{ts,tsx}': ['stylelint --cache'],
+  './**/*.md': ['cspell', 'prettier -c'],
 };
