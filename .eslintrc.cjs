fe389fc28afdf2faf83b8dace38d367612e2c385
wakamsha <wakamsha@gmail.com>
2023-07-11 16:10:20 +0900

    :cyclone: eslint rules の並び順を flat config 風味に変更する

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,68 +23,14 @@ module.exports = {
     project: ['./{apps,packages}/**/tsconfig.json'],
   },
   ignorePatterns: ['.eslintrc.*', 'dist*', 'bin*', '*.config.*'],
-  plugins: ['react', 'react-hooks'],
+  plugins: ['jsdoc', 'react', 'react-hooks'],
   rules: {
+    // ----------------
+    // Best practices
+    // ----------------
     // Enable
-    '@typescript-eslint/ban-types': ['error'],
-    '@typescript-eslint/consistent-type-definitions': ['warn', 'type'],
-    '@typescript-eslint/consistent-type-imports': [
-      'error',
-      {
-        fixStyle: 'inline-type-imports',
-      },
-    ],
-    '@typescript-eslint/dot-notation': ['error'],
-    '@typescript-eslint/no-floating-promises': [
-      'error',
-      {
-        ignoreIIFE: true,
-      },
-    ],
-    '@typescript-eslint/no-misused-promises': [
-      'error',
-      {
-        checksVoidReturn: false,
-      },
-    ],
-    '@typescript-eslint/no-unused-expressions': [
-      'error',
-      {
-        allowShortCircuit: true,
-        allowTernary: true,
-        allowTaggedTemplates: true,
-      },
-    ],
-    '@typescript-eslint/no-useless-constructor': ['error'],
-    '@typescript-eslint/restrict-template-expressions': [
-      'error',
-      {
-        allowBoolean: true,
-      },
-    ],
-    '@typescript-eslint/switch-exhaustiveness-check': ['error'],
-    'react/function-component-definition': [
-      'error',
-      {
-        namedComponents: 'arrow-function',
-        unnamedComponents: 'arrow-function',
-      },
-    ],
-    'react/jsx-filename-extension': [
-      'error',
-      {
-        extensions: ['.tsx'],
-      },
-    ],
-    'react/jsx-key': ['error'],
-    'react/jsx-no-target-blank': ['error'],
-    'react/jsx-props-no-spreading': ['warn'],
-    'react/sort-comp': ['error'],
-    'react-hooks/exhaustive-deps': ['error'],
-    'react-hooks/rules-of-hooks': ['error'],
     'arrow-body-style': ['error', 'as-needed'],
     'func-names': ['error'],
-    'import/no-default-export': ['error'],
     'lines-between-class-members': [
       'error',
       'always',
@@ -127,37 +73,7 @@ module.exports = {
         allowTernary: true,
       },
     ],
-
     // Disable
-    '@typescript-eslint/explicit-function-return-type': ['off'],
-    '@typescript-eslint/explicit-module-boundary-types': ['off'],
-    '@typescript-eslint/no-explicit-any': ['off'],
-    '@typescript-eslint/no-unsafe-call': ['off'], // tsconfig にて設定した alias path を認識できないため無効化する。
-    '@typescript-eslint/no-unsafe-argument': ['off'], // tsconfig にて設定した alias path を認識できないため無効化する。
-    '@typescript-eslint/no-unsafe-assignment': ['off'], // tsconfig にて設定した alias path や画像ファイルを正しく認識できず any 型と誤認するため無効化する。
-    '@typescript-eslint/no-unsafe-member-access': ['off'], // tsconfig にて設定した alias path を認識できないため無効化する。
-    '@typescript-eslint/no-unsafe-return': ['off'], // tsconfig にて設定した alias path を認識できないため無効化する。
-    '@typescript-eslint/no-use-before-define': ['off'],
-    'import/extensions': ['off'], // import path に拡張子を含めることを強制するため無効化する。
-    'import/no-extraneous-dependencies': ['off'], // プロジェクトルートにある node モジュールの import を指摘するため無効化する。
-    'import/no-unresolved': ['off'], // tsconfig にて設定した alias path を認識できないため無効化する。
-    'import/prefer-default-export': ['off'],
-    'jsx-a11y/accessible-emoji': ['off'],
-    'jsx-a11y/control-has-associated-label': ['off'],
-    'jsx-a11y/click-events-have-key-events': ['off'],
-    'jsx-a11y/interactive-supports-focus': ['off'],
-    'jsx-a11y/label-has-associated-control': ['off'],
-    'jsx-a11y/media-has-caption': ['off'],
-    'jsx-a11y/mouse-events-have-key-events': ['off'],
-    'jsx-a11y/no-autofocus': ['off'],
-    'jsx-a11y/no-noninteractive-element-interactions': ['off'],
-    'react/button-has-type': ['off'],
-    'react/display-name': ['off'],
-    'react/jsx-indent': ['off'],
-    'react/no-array-index-key': ['off'],
-    'react/prop-types': ['off'],
-    'react/react-in-jsx-scope': ['off'],
-    'react/require-default-props': ['off'],
     'class-methods-use-this': ['off'],
     'consistent-return': ['off'], // TypeScript のフロー解析を考慮できないため false positive が発生する
     'default-case': ['off'], // switch の条件式に渡される値の型次第では default は不要となる。
@@ -170,8 +86,141 @@ module.exports = {
     'no-plusplus': ['off'],
     'no-shadow': ['off'],
     'no-throw-literal': ['off'],
+
+    // ----------------
+    // typescript
+    // ----------------
+    // Enable
+    '@typescript-eslint/ban-types': ['error'],
+    '@typescript-eslint/consistent-type-definitions': ['warn', 'type'],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    '@typescript-eslint/dot-notation': ['error'],
+    '@typescript-eslint/no-floating-promises': [
+      'error',
+      {
+        ignoreIIFE: true,
+      },
+    ],
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: false,
+      },
+    ],
+    '@typescript-eslint/no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+        allowTaggedTemplates: true,
+      },
+    ],
+    '@typescript-eslint/no-useless-constructor': ['error'],
+    '@typescript-eslint/restrict-template-expressions': [
+      'error',
+      {
+        allowBoolean: true,
+      },
+    ],
+    '@typescript-eslint/switch-exhaustiveness-check': ['error'],
+    // Disable
+    '@typescript-eslint/explicit-function-return-type': ['off'],
+    '@typescript-eslint/explicit-module-boundary-types': ['off'],
+    '@typescript-eslint/no-explicit-any': ['off'],
+    '@typescript-eslint/no-unsafe-call': ['off'], // tsconfig にて設定した alias path を認識できないため無効化する。
+    '@typescript-eslint/no-unsafe-argument': ['off'], // tsconfig にて設定した alias path を認識できないため無効化する。
+    '@typescript-eslint/no-unsafe-assignment': ['off'], // tsconfig にて設定した alias path や画像ファイルを正しく認識できず any 型と誤認するため無効化する。
+    '@typescript-eslint/no-unsafe-member-access': ['off'], // tsconfig にて設定した alias path を認識できないため無効化する。
+    '@typescript-eslint/no-unsafe-return': ['off'], // tsconfig にて設定した alias path を認識できないため無効化する。
+    '@typescript-eslint/no-use-before-define': ['off'],
+
+    // ----------------
+    // import
+    // ----------------
+    // Enable
+    'import/no-default-export': ['error'],
+    // Disable
+    'import/extensions': ['off'], // import path に拡張子を含めることを強制するため無効化する。
+    'import/no-extraneous-dependencies': ['off'], // プロジェクトルートにある node モジュールの import を指摘するため無効化する。
+    'import/no-unresolved': ['off'], // tsconfig にて設定した alias path を認識できないため無効化する。
+    'import/prefer-default-export': ['off'],
+
+    // ----------------
+    // react
+    // ----------------
+    // Enable
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.tsx'],
+      },
+    ],
+    'react/jsx-key': ['error'],
+    'react/jsx-no-target-blank': ['error'],
+    'react/jsx-props-no-spreading': ['warn'],
+    'react/sort-comp': ['error'],
+    'react-hooks/exhaustive-deps': ['error'],
+    'react-hooks/rules-of-hooks': ['error'],
+    // Disable
+    'react/button-has-type': ['off'],
+    'react/display-name': ['off'],
+    'react/jsx-indent': ['off'],
+    'react/no-array-index-key': ['off'],
+    'react/prop-types': ['off'],
+    'react/react-in-jsx-scope': ['off'],
+    'react/require-default-props': ['off'],
+
+    // ----------------
+    // jsx-a11y
+    // ----------------
+    // Disable
+    'jsx-a11y/accessible-emoji': ['off'],
+    'jsx-a11y/control-has-associated-label': ['off'],
+    'jsx-a11y/click-events-have-key-events': ['off'],
+    'jsx-a11y/interactive-supports-focus': ['off'],
+    'jsx-a11y/label-has-associated-control': ['off'],
+    'jsx-a11y/media-has-caption': ['off'],
+    'jsx-a11y/mouse-events-have-key-events': ['off'],
+    'jsx-a11y/no-autofocus': ['off'],
+    'jsx-a11y/no-noninteractive-element-interactions': ['off'],
   },
   overrides: [
+    // ----------------
+    // Best practice
+    // ----------------
+    {
+      files: ['*.js', '*.mjs'],
+      rules: {
+        'no-underscore-dangle': ['off'],
+      },
+    },
+
+    // ----------------
+    // typescript
+    // ----------------
+    {
+      files: ['*.js', '*.mjs'],
+      rules: {
+        '@typescript-eslint/ban-ts-comment': ['off'],
+        '@typescript-eslint/no-var-requires': ['off'],
+      },
+    },
+
+    // ----------------
+    // import
+    // ----------------
     {
       files: ['./**/ambience.d.ts'],
       rules: {
@@ -179,20 +228,25 @@ module.exports = {
       },
     },
     {
+      files: ['*.js', '*.mjs'],
+      rules: {
+        'import/no-default-export': ['off'],
+      },
+    },
+
+    // ----------------
+    // react-hooks
+    // ----------------
+    {
       files: ['./**/stores/**/*.ts'],
       rules: {
         'react-hooks/rules-of-hooks': ['off'],
       },
     },
-    {
-      files: ['*.js', '*.mjs'],
-      rules: {
-        '@typescript-eslint/ban-ts-comment': ['off'],
-        '@typescript-eslint/no-var-requires': ['off'],
-        'no-underscore-dangle': ['off'],
-        'import/no-default-export': ['off'],
-      },
-    },
+
+    // ----------------
+    // jest
+    // ----------------
     {
       files: ['*.test.*'],
       extends: ['plugin:jest/recommended'],
