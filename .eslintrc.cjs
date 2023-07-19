ec595771130edfa06f287b3d971dec0cb3e44a20
wakamsha <wakamsha@gmail.com>
2023-07-20 02:29:04 +0900

    :wrench: typescript-eslint のルールを別ファイルとして切り出す

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,8 +11,7 @@ module.exports = {
     './eslint-rules/react.cjs',
     './eslint-rules/react-hooks.cjs',
     './eslint-rules/jest.cjs',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    './eslint-rules/typescript.cjs',
     'prettier',
   ],
   env: {
@@ -38,69 +37,5 @@ module.exports = {
     'no-undef': ['off'],
     'no-use-before-define': ['off'],
     'no-shadow': ['off'],
-
-    // ----------------
-    // typescript
-    // ----------------
-    // Enable
-    '@typescript-eslint/ban-types': ['error'],
-    '@typescript-eslint/consistent-type-definitions': ['warn', 'type'],
-    '@typescript-eslint/consistent-type-imports': [
-      'error',
-      {
-        fixStyle: 'inline-type-imports',
-      },
-    ],
-    '@typescript-eslint/dot-notation': ['error'],
-    '@typescript-eslint/no-floating-promises': [
-      'error',
-      {
-        ignoreIIFE: true,
-      },
-    ],
-    '@typescript-eslint/no-misused-promises': [
-      'error',
-      {
-        checksVoidReturn: false,
-      },
-    ],
-    '@typescript-eslint/no-unused-expressions': [
-      'error',
-      {
-        allowShortCircuit: true,
-        allowTernary: true,
-        allowTaggedTemplates: true,
-      },
-    ],
-    '@typescript-eslint/no-useless-constructor': ['error'],
-    '@typescript-eslint/restrict-template-expressions': [
-      'error',
-      {
-        allowBoolean: true,
-      },
-    ],
-    '@typescript-eslint/switch-exhaustiveness-check': ['error'],
-    // Disable
-    '@typescript-eslint/explicit-function-return-type': ['off'],
-    '@typescript-eslint/explicit-module-boundary-types': ['off'],
-    '@typescript-eslint/no-explicit-any': ['off'],
-    '@typescript-eslint/no-unsafe-call': ['off'], // tsconfig にて設定した alias path を認識できないため無効化する。
-    '@typescript-eslint/no-unsafe-argument': ['off'], // tsconfig にて設定した alias path を認識できないため無効化する。
-    '@typescript-eslint/no-unsafe-assignment': ['off'], // tsconfig にて設定した alias path や画像ファイルを正しく認識できず any 型と誤認するため無効化する。
-    '@typescript-eslint/no-unsafe-member-access': ['off'], // tsconfig にて設定した alias path を認識できないため無効化する。
-    '@typescript-eslint/no-unsafe-return': ['off'], // tsconfig にて設定した alias path を認識できないため無効化する。
-    '@typescript-eslint/no-use-before-define': ['off'],
   },
-  overrides: [
-    // ----------------
-    // typescript
-    // ----------------
-    {
-      files: ['*.js', '*.mjs'],
-      rules: {
-        '@typescript-eslint/ban-ts-comment': ['off'],
-        '@typescript-eslint/no-var-requires': ['off'],
-      },
-    },
-  ],
 };
