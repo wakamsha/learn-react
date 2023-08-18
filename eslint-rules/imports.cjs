48214fa47ef9f7b6827b2700abf27f2692255c8f
wakamsha <wakamsha@gmail.com>
2023-08-18 15:11:10 +0900

    :wrench: import/order を無効化する

--- eslint-rules/imports.cjs
+++ eslint-rules/imports.cjs
@@ -121,8 +121,8 @@ module.exports = {
 
     // ensure absolute imports are above relative imports and that unassigned imports are ignored
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
-    // TODO: enforce a stricter convention in module import order?
-    'import/order': ['error', { groups: [['builtin', 'external', 'internal']] }],
+    // prettier-plugin-organize-imports に委譲するため無効化する。
+    'import/order': ['off'],
 
     // Require a newline after the last import/require in a group
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/newline-after-import.md
