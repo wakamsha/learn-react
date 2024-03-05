d2c9225c35bb303de5bb96ed6ae29b11987d9997
wakamsha <wakamsha@gmail.com>
2024-03-05 15:59:01 +0900

    :wrench: update es6

--- eslint-rules/es6.js
+++ eslint-rules/es6.js
@@ -2,7 +2,6 @@ export default {
   rules: {
     // enforces no braces where they can be omitted
     // https://eslint.org/docs/rules/arrow-body-style
-    // TODO: enable requireReturnForObjectLiteral?
     'arrow-body-style': [
       'error',
       'as-needed',
@@ -19,9 +18,16 @@ export default {
     // https://eslint.org/docs/rules/arrow-spacing
     'arrow-spacing': ['error', { before: true, after: true }],
 
+    // verify super() callings in constructors
+    'constructor-super': 'error',
+
     // enforce the spacing around the * in generator functions
     // https://eslint.org/docs/rules/generator-star-spacing
     'generator-star-spacing': ['error', { before: false, after: true }],
+
+    // disallow modifying variables of class declarations
+    // https://eslint.org/docs/rules/no-class-assign
+    'no-class-assign': 'error',
 
     // disallow arrow functions where they could be confused with comparisons
     // https://eslint.org/docs/rules/no-confusing-arrow
@@ -32,10 +38,21 @@ export default {
       },
     ],
 
+    // disallow modifying variables that are declared using const
+    'no-const-assign': ['error'],
+
+    // disallow duplicate class members
+    // https://eslint.org/docs/rules/no-dupe-class-members
+    'no-dupe-class-members': ['error'],
+
     // disallow importing from the same path more than once
     // https://eslint.org/docs/rules/no-duplicate-imports
     // replaced by https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-duplicates.md
-    'no-duplicate-imports': 'off',
+    'no-duplicate-imports': ['off'],
+
+    // disallow symbol constructor
+    // https://eslint.org/docs/rules/no-new-symbol
+    'no-new-symbol': ['error'],
 
     // Disallow specified names in exports
     // https://eslint.org/docs/rules/no-restricted-exports
@@ -58,15 +75,19 @@ export default {
           // {
           //   name: 'path-to-foo',
           //   importNames: ['module-name'],
-          //   message: '代わりに `path-to-bar` の `module-name` をお使いください。',
+          //   message: 'Use `module-name` in `path-to-bar` instead.',
           // },
         ],
       },
     ],
 
+    // disallow to use this/super before super() calling in constructors.
+    // https://eslint.org/docs/rules/no-this-before-super
+    'no-this-before-super': 'error',
+
     // disallow useless computed property keys
     // https://eslint.org/docs/rules/no-useless-computed-key
-    'no-useless-computed-key': 'error',
+    'no-useless-computed-key': ['error'],
 
     // disallow unnecessary constructor
     // https://eslint.org/docs/rules/no-useless-constructor
@@ -84,7 +105,7 @@ export default {
     ],
 
     // require let or const instead of var
-    'no-var': 'error',
+    'no-var': ['error'],
 
     // require method and property shorthand syntax for object literals
     // https://eslint.org/docs/rules/object-shorthand
@@ -136,23 +157,23 @@ export default {
 
     // disallow parseInt() in favor of binary, octal, and hexadecimal literals
     // https://eslint.org/docs/rules/prefer-numeric-literals
-    'prefer-numeric-literals': 'error',
+    'prefer-numeric-literals': ['error'],
 
     // suggest using Reflect methods where applicable
     // https://eslint.org/docs/rules/prefer-reflect
-    'prefer-reflect': 'off',
+    'prefer-reflect': ['off'],
 
     // use rest parameters instead of arguments
     // https://eslint.org/docs/rules/prefer-rest-params
-    'prefer-rest-params': 'error',
+    'prefer-rest-params': ['error'],
 
     // suggest using the spread syntax instead of .apply()
     // https://eslint.org/docs/rules/prefer-spread
-    'prefer-spread': 'error',
+    'prefer-spread': ['error'],
 
     // suggest using template literals instead of string concatenation
     // https://eslint.org/docs/rules/prefer-template
-    'prefer-template': 'error',
+    'prefer-template': ['error'],
 
     // enforce spacing between object rest-spread
     // https://eslint.org/docs/rules/rest-spread-spacing
@@ -172,11 +193,11 @@ export default {
 
     // require a Symbol description
     // https://eslint.org/docs/rules/symbol-description
-    'symbol-description': 'error',
+    'symbol-description': ['error'],
 
     // enforce usage of spacing in template strings
     // https://eslint.org/docs/rules/template-curly-spacing
-    'template-curly-spacing': 'error',
+    'template-curly-spacing': ['error'],
 
     // enforce spacing around the * in yield* expressions
     // https://eslint.org/docs/rules/yield-star-spacing
