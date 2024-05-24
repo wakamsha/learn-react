b7dd5714eb8113dd3ad48c4d723bc2cc08d54add
wakamsha <wakamsha@gmail.com>
2024-05-24 15:38:27 +0900

    :construction: typescript config を作成する

--- packages/core/eslint.config.js
+++ packages/core/eslint.config.js
@@ -1,8 +1,22 @@
 import essentials from '@learn-react/eslint-config/essentials';
+import typescript from '@learn-react/eslint-config/typescript';
 
 export default [
   {
     files: ['**/*.{ts,tsx}'],
   },
   essentials,
+
+  typescript,
+  // {
+  //   ...typescript,
+  //   languageOptions: {
+  //     ...typescript.languageOptions,
+  //     parserOptions: {
+  //       ...typescript.languageOptions.parserOptions,
+  //       project: ['./tsconfig.json'],
+  //       // tsconfigRootDir: import.meta.dirname,
+  //     },
+  //   },
+  // },
 ];
---
+++ packages/eslint-config/configs/typescript.js
@@ -0,0 +1,17 @@
+import typescriptEslintParser from '@typescript-eslint/parser';
+import { defineFlatConfig } from 'eslint-define-config';
+import typescriptConfig from '../rules/typescript.js';
+
+export default defineFlatConfig({
+  languageOptions: {
+    parser: typescriptEslintParser,
+    parserOptions: {
+      project: true,
+      // tsconfigRootDir: import.meta.dirname,
+    },
+  },
+
+  ...typescriptConfig,
+
+  files: ['**/*.@(ts|tsx|cts|mts)'],
+});
