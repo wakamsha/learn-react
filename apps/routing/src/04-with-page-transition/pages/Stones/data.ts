13b2de3e40055489f62349511f0bb76b13c26f0c
wakamsha <wakamsha@gmail.com>
2024-10-17 13:42:15 +0900

    :recycle: Toast の実装から循環参照を除去する

---
+++ apps/routing/src/04-with-page-transition/pages/Stones/data.ts
@@ -0,0 +1,28 @@
+import { type MemberType } from './type';
+
+export const MemberData: MemberType[] = [
+  {
+    id: 'jagger',
+    nameJa: 'ミック・ジャガー',
+    nameEn: 'Mick Jagger',
+    family: 'Vocal',
+  },
+  {
+    id: 'richards',
+    nameJa: 'キース・リチャーズ',
+    nameEn: 'Keith Richards',
+    family: 'Guitar',
+  },
+  {
+    id: 'wood',
+    nameJa: 'ロン・ウッド',
+    nameEn: 'Ronnie Wood',
+    family: 'Guitar',
+  },
+  {
+    id: 'watts',
+    nameJa: 'チャーリー・ワッツ',
+    nameEn: 'Charlie Watts',
+    family: 'Drum',
+  },
+];
