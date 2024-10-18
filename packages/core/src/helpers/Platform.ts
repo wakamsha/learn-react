c431321c5b64008088e752f7aa9da62ae914ecdd
wakamsha <wakamsha@gmail.com>
2024-10-18 14:00:22 +0900

    :cyclone: fix

--- packages/core/src/helpers/Platform.ts
+++ packages/core/src/helpers/Platform.ts
@@ -8,5 +8,8 @@ export function isMac() {
 }
 
 function testPlatform(re: RegExp) {
-  return re.test(window.navigator.userAgentData?.platform ?? '');
+  // userAgentData は 2024年10月時点で Chromium にのみ実装されているため、
+  // それ以外の実行環境では引き続き userAgent を使用します。
+  // eslint-disable-next-line @typescript-eslint/no-deprecated
+  return re.test(window.navigator.userAgentData?.platform ?? window.navigator.platform);
 }
