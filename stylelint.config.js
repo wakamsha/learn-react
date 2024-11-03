95b6187ac87c2e9b3c26951be48830d198aabde3
YAMADA Naoki <wakamsha@gmail.com>
2024-11-03 11:36:21 +0900

    :pill: @wakamsha/stylelint-config v2 の設計に追従する (#1883)

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,10 +1,4 @@
 export default {
-  extends: ['@wakamsha/stylelint-config'],
+  extends: ['@wakamsha/stylelint-config/essentials', '@wakamsha/stylelint-config/css-in-js'],
   ignoreFiles: ['**/node_modules/**'],
-  customSyntax: '@stylelint/postcss-css-in-js',
-  rules: {
-    'function-name-case': null,
-    'function-no-unknown': null,
-    'value-keyword-case': null,
-  },
 };
