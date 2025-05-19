ce02f43695d7bd34a461bd533ef7d9d80069814d
wakamsha <wakamsha@gmail.com>
2025-05-19 21:18:07 +0900

    chore(try): getting started

---
+++ packages/try/src/ECharts/GettingStarted.story.tsx
@@ -0,0 +1,55 @@
+import { init, type ECharts } from 'echarts';
+import { type ECBasicOption } from 'echarts/types/dist/shared';
+import { useEffect, useRef, type FC } from 'react';
+
+/**
+ *
+ * @see {@link https://echarts.apache.org/handbook/en/get-started/ Getting Started}
+ */
+export const Story: FC = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const chartRef = useRef<ECharts>(null);
+
+  useEffect(() => {
+    if (!containerRef.current) {
+      return;
+    }
+
+    chartRef.current = init(containerRef.current);
+
+    const option: ECBasicOption = {
+      title: {
+        text: 'ECharts Getting Started Example',
+      },
+      tooltip: {},
+      legend: {
+        data: ['sales'],
+      },
+      xAxis: {
+        data: ['Shirts', 'Cardigans', 'Chiffons', 'Pants', 'Heels', 'Socks'],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: 'sales',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20],
+        },
+      ],
+    };
+
+    chartRef.current.setOption(option);
+
+    return () => {
+      chartRef.current?.dispose();
+    };
+  }, []);
+
+  return (
+    <>
+      <h2>Getting Started with ECharts</h2>
+      <div ref={containerRef} style={{ width: '100%', height: '400px' }} />
+    </>
+  );
+};
