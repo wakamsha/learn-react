c4b764e3392ab9e390b1b3ac6b9b6358053a91f8
wakamsha <wakamsha@gmail.com>
2026-06-30 16:25:53 +0900

    feat(search-ui-rr): implement debouncing
    fix(search-ui-rr): fix lint errors & write JSDoc

--- apps/search-ui-rr/src/routes/home/route.tsx
+++ apps/search-ui-rr/src/routes/home/route.tsx
@@ -1,6 +1,7 @@
-import { ChangeEvent, FC, SubmitEvent, useState } from 'react';
-import { Form, LoaderFunctionArgs, useLoaderData, useNavigation, useSubmit } from 'react-router';
+import { type ChangeEvent, type FC, useState } from 'react';
+import { Form, type LoaderFunctionArgs, useLoaderData, useNavigation, useSubmit } from 'react-router';
 import { getContacts } from '../../data';
+import { useDebouncedCallback } from '../../hooks/useDebouncedCallback';
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const url = new URL(request.url);
@@ -15,6 +16,17 @@ export async function loader({ request }: LoaderFunctionArgs) {
   };
 }
 
+/**
+ * React Router 7 の機能を活用したインクリメンタルサーチ UI のデモ。
+ *
+ * Form コンポーネントと useSubmit フックを組み合わせることで、フォームの入力値が変更されるたびにサーバーにリクエストを送信し、結果を取得して表示することができる。
+ *
+ * また、自作した useDebouncedCallback フックを使用して、入力値の変更が一定時間続いた場合にのみサーバーにリクエストを送信するようにしている。
+ * これにより、ユーザーが入力中にサーバーへのリクエストが過剰に送信されることを防ぎ、パフォーマンスの向上につながる。
+ *
+ * さらに、useNavigation フックを使用して現在のナビゲーション状態を取得し、検索中であることを示すインジケーターを表示することができる。
+ * このように、React Router 7 の機能を活用することで、シンプルかつ効率的なインクリメンタルサーチ UI を実装することができる。
+ */
 export const Component: FC = () => {
   const { contacts, q } = useLoaderData<typeof loader>();
 
@@ -30,12 +42,16 @@ export const Component: FC = () => {
     setQuery(event.target.value);
   };
 
-  const handleFormChange = async (event: SubmitEvent<HTMLFormElement>) => {
+  const debouncedSubmit = useDebouncedCallback(async (form: HTMLFormElement) => {
     const isFirstSearch = q === null;
 
-    await submit(event.currentTarget, {
+    await submit(form, {
       replace: !isFirstSearch,
     });
+  }, 300);
+
+  const handleFormChange = (event: ChangeEvent<HTMLFormElement>) => {
+    debouncedSubmit(event.currentTarget);
   };
 
   return (
